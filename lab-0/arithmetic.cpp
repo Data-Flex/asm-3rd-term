7f6ea94d0c2145bff772a2f17ed9b5704aa51e6a
Data-Flex <DreamTim2004@mail.ru>
2023-09-06 23:18:22 +0300

    fix first report

---
+++ lab-0/arithmetic.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int x = 0, y = 0, z = 0, a = 0;
+int main() {
+	x = 9;
+	y = 5;
+	a = 2;
+	z = x * y * a;
+	cout << z << endl;
+}
---
+++ lab-0/arithmetic.s
@@ -0,0 +1,142 @@
+	.file	"arithmetic.cpp"
+	.text
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.globl	x
+	.bss
+	.align 4
+	.type	x, @object
+	.size	x, 4
+x:
+	.zero	4
+	.globl	y
+	.align 4
+	.type	y, @object
+	.size	y, 4
+y:
+	.zero	4
+	.globl	z
+	.align 4
+	.type	z, @object
+	.size	z, 4
+z:
+	.zero	4
+	.globl	a
+	.align 4
+	.type	a, @object
+	.size	a, 4
+a:
+	.zero	4
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1731:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$9, x(%rip)
+	movl	$5, y(%rip)
+	movl	$2, a(%rip)
+	movl	x(%rip), %edx
+	movl	y(%rip), %eax
+	imull	%eax, %edx
+	movl	a(%rip), %eax
+	imull	%edx, %eax
+	movl	%eax, z(%rip)
+	movl	z(%rip), %eax
+	movl	%eax, %esi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZNSolsEi@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1731:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2231:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L5
+	cmpl	$65535, -8(%rbp)
+	jne	.L5
+	leaq	_ZStL8__ioinit(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rax
+	movq	%rax, %rdx
+	leaq	_ZStL8__ioinit(%rip), %rax
+	movq	%rax, %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L5:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2231:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_x, @function
+_GLOBAL__sub_I_x:
+.LFB2232:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2232:
+	.size	_GLOBAL__sub_I_x, .-_GLOBAL__sub_I_x
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_x
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
